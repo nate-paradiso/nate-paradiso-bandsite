58f1e7bc0d5a26e39fa0a7e7a0ee952d47ef4e83
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-04 14:59:02 -0700

    saving precious data

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,21 +1,44 @@
 const shows = [
     {
-      dateLabel: "Date",
-      date: "Mon Sept 06 2021",
-      venueLabel: "Venue",
-      venue: "Ronald Lane",
-      locationLabel: "Location",
-      location: "San Francisco, CA",
-      button: "Buy Tickets"      
+        dateLabel: "Date",
+        date: "Mon Sept 06 2021",
+        venueLabel: "Venue",
+        venue: "Ronald Lane",
+        locationLabel: "Location",
+        location: "San Francisco, CA",
+        button: "Buy Tickets"      
     },
     {
-      title: "Awful site!",
-      content: "This is really a terrible site I am so disgusted by you!",
-      profile_pic: "./patrick.jpg"
+        dateLabel: "Date",
+        date: "Tue Sept 21 2021",
+        venueLabel: "Venue",
+        venue: "Pier 3 East",
+        locationLabel: "Location",
+        location: "San Francisco, CA",
+        button: "Buy Tickets" 
+    },
+    {
+        dateLabel: "Date",
+        date: "Fri Oct 15 2021",
+        venueLabel: "Venue",
+        venue: "View Lounge",
+        locationLabel: "Location",
+        location: "San Francisco, CA",
+        button: "Buy Tickets" 
+    },
+    {
+        dateLabel: "Date",
+        date: "Sat Nov 06 2021",
+        venueLabel: "Venue",
+        venue: "Hyatt Agency",
+        locationLabel: "Location",
+        location: "San Francisco, CA",
+        button: "Buy Tickets" 
     },
   ]
-function addShows(){
-    // const showsCard = document.querySelector(".shows__container");
+function addShows(event){
+    
+    const showsContainer = document.querySelector(".shows__container");
     for(let i = 0; i < shows.length; i++){
         const showsCard = document.createElement("div");
         const showsDateLabel = document.createElement("p");
@@ -28,17 +51,38 @@ function addShows(){
         const showsLocation = document.createElement("p");
         
         const buyNowButton = document.createElement("button");
+
         showsCard.classList.add("shows__container");
         showsDateLabel.classList.add("shows__label");
         showsVenueLabel.classList.add("shows__label");
         showsLocationLabel.classList.add("shows__label");
-        showsDate.innerText = shows[i].date
+        showsDate.classList.add("shows__date");
+        showsVenue.classList.add("shows__text");
+        showsLocation.classList.add("shows__text");
+        buyNowButton.classList.add("shows__buy-button");
+
+        
+        showsDateLabel.innerText = shows[i].dateLabel;
+        showsDate.innerText = shows[i].date;
+        showsVenueLabel.innerText = shows[i].venueLabel;
+        showsVenue.innerText = shows[i].venue;
+        showsLocationLabel.innerText = shows[i].locationLabel;
+        showsLocation.innerText = shows[i].location;
+        buyNowButton.innerText = shows[i].button;
+
+
         showsCard.appendChild(showsDate);
+        showsCard.appendChild(showsDateLabel);
+        showsCard.appendChild(showsVenueLabel);
+        showsCard.appendChild(showsVenue);
+        showsCard.appendChild(showsLocationLabel);
+        showsCard.appendChild(showsLocation);
+        showsCard.appendChild(buyNowButton);
+        showsContainer.appendChild(showsCard);
+
 
         
     }
+    
 }
-setTimeout(
-    addShows,
-    2000
-  )
+addShows()
