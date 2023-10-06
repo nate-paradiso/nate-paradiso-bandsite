f593eba4daa8f05df1adc151b8cbd9b075ae6357
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-06 09:59:59 -0700

    fixed position of comment box

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -30,25 +30,29 @@ function addDataDisplay() {
     const avatar = document.createElement("div");
     avatar.classList.add("comments-container__avatar");
     commentContainer.appendChild(avatar)
+ 
+    const commentAllText = document.createElement("div");
+    commentAllText.classList.add("comments-container__all-text");
+    commentContainer.appendChild(commentAllText)
+    
+    const commentTextTimeContainer = document.createElement("div");
+    commentTextTimeContainer.classList.add("comments-container__text--time");
+    commentAllText.appendChild(commentTextTimeContainer)
     
     const commentTextContainer = document.createElement("div");
     commentTextContainer.classList.add("comments-container__text");
-    commentContainer.appendChild(commentTextContainer)
-
-    const commentTextTimeContainer = document.createElement("div");
-    commentTextContainer.classList.add("comments-container__text--time");
-    commentContainer.appendChild(commentTextTimeContainer)
+    commentAllText.appendChild(commentTextContainer)
 
     const name = document.createElement("p");
-    name.classList.add("comment-container__text--name");
+    name.classList.add("comment-container__text--name-label");
     name.textContent = formData.name;
     commentTextTimeContainer.appendChild(name);
 
     const timestamp = new Date().toLocaleString();
     const timestampTag = document.createElement("p");
-    timestampTag.classList.add("comment-container__text--time");
+    timestampTag.classList.add("comment-container__text--time-label");
     timestampTag.textContent = timestamp;
-    name.appendChild(timestampTag);
+    commentTextTimeContainer.appendChild(timestampTag);
 
     const comment = document.createElement("p");
     comment.classList.add("comment-container__text--comment");
