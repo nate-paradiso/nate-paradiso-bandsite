a22f7d24eab03106a142aeefcfe7da7a335e217e
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-05 17:43:07 -0700

    brain dead saving for the night

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -35,10 +35,20 @@ function addDataDisplay() {
     commentTextContainer.classList.add("comments-container__text");
     commentContainer.appendChild(commentTextContainer)
 
+    const commentTextTimeContainer = document.createElement("div");
+    commentTextContainer.classList.add("comments-container__text--time");
+    commentContainer.appendChild(commentTextTimeContainer)
+
     const name = document.createElement("p");
     name.classList.add("comment-container__text--name");
     name.textContent = formData.name;
-    commentTextContainer.appendChild(name);
+    commentTextTimeContainer.appendChild(name);
+
+    const timestamp = new Date().toLocaleString();
+    const timestampTag = document.createElement("p");
+    timestampTag.classList.add("comment-container__text--time");
+    timestampTag.textContent = timestamp;
+    name.appendChild(timestampTag);
 
     const comment = document.createElement("p");
     comment.classList.add("comment-container__text--comment");
