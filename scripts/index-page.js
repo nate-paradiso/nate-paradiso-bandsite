941f14e067e2b1bfe7eb7b55ac1d1ffc454737ad
Nate Paradiso <ipfcolo05@gmail.com>
2023-10-06 15:08:43 -0700

    added defualt comments to array

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,23 @@
 const form = document.getElementById("myForm");
 const formComments = document.getElementById("formComments");
-const formCommentArray = [];
+const formCommentArray = [
+  {
+    name: "Mile Acosta",
+    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    timeStamp: "02/17/2021",
+  },
+  {
+    name: "Emilie Beach",
+    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    timeStamp: "01/09/2021",
+  },
+  {
+    name: "Connor Walton",
+    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    timeStamp: "12/20/2020",
+  },
+ 
+];
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -11,6 +28,7 @@ form.addEventListener("submit", function (event) {
     const formData = {
         name, 
         comment,
+      
     };
 
     formCommentArray.push(formData);
@@ -19,9 +37,7 @@ form.addEventListener("submit", function (event) {
     addDataDisplay();
 });
 
-function addDataDisplay() {
-    formComments.innerHTML = "";
-
+function loadComments() {
   formCommentArray.forEach((formData) => {
 
     const commentContainer = document.createElement("div");
@@ -44,18 +60,18 @@ function addDataDisplay() {
     commentAllText.appendChild(commentTextContainer)
 
     const name = document.createElement("p");
-    name.classList.add("comment-container__text--name-label");
+    name.classList.add("comments-container__text--name-label");
     name.textContent = formData.name;
     commentTextTimeContainer.appendChild(name);
 
-    const timestamp = new Date().toLocaleString();
-    const timestampTag = document.createElement("p");
-    timestampTag.classList.add("comment-container__text--time-label");
-    timestampTag.textContent = timestamp;
-    commentTextTimeContainer.appendChild(timestampTag);
+    const timeStamp = new Date().toLocaleDateString();
+    const timeStampTag = document.createElement("p");
+    timeStampTag.classList.add("comments-container__text--time-label");
+    timeStampTag.textContent = timeStamp;
+    commentTextTimeContainer.appendChild(timeStampTag);
 
     const comment = document.createElement("p");
-    comment.classList.add("comment-container__text--comment");
+    comment.classList.add("comments-container__text--comment");
     comment.textContent = formData.comment;
     commentTextContainer.appendChild(comment);
 
@@ -63,5 +79,13 @@ function addDataDisplay() {
 
     formComments.appendChild(commentContainer)
   });
+}
+loadComments()
+
+function addDataDisplay() {
+    formComments.innerHTML = "";
+    formCommentArray.sort();
+    loadComments();
+  
 
 }
